6b52fe89e4aa2cdb2573121341aaeb6821698a9a
Gemma Massó <gmasso@visual-engin.com>
2019-07-08 11:57:35 +0200

    Initial project

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyC830Br6qrQzkV4baY6VQ9dDuLG-I2L0CU';
+
+
+// Create a new component. This component should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
